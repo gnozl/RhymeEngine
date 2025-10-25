a7fc6139a764eb58dcaa6025002b80d26e67fc05
GnozL <gggonzalez000@gmail.com>
2025-10-25 16:52:35 -0700

    fix main

--- src/main.cpp
+++ src/main.cpp
@@ -13,22 +13,24 @@ int main()
     //TODO: Find rhymes inside text / end of lines
     //TODO: Print out the text, with rhymes highlighted
 
-    std::cout << "Intro message\n";
-
     RhymeEngine rhymeEngine;
+    rhymeEngine.run();
 
-    const std::string filename = rhymeEngine.getFileNameFromUser();
-    rhymeEngine.loadTextFile(filename);
-    std::string title;
-    std::cin >> title;
-    Text text = rhymeEngine.createText(title, filename); //TODO: This does a lot of stuff
+    // std::cout << "Intro message\n";
 
-    for (Line * line : text.getLines()) {
-        Word * temp_word = line->getFinalWord();
-        rhymeEngine.matchRhymeToColors(temp_word);
-        rhymeEngine.printColorText();
+    // const std::string filename = rhymeEngine.getFileNameFromUser();
+    // rhymeEngine.loadTextFile(filename);
+    // std::string title;
+    // std::cin >> title;
+    // Text text = rhymeEngine.createText(title, filename); //TODO: This does a lot of stuff
 
-        }
+
+    // for (Line * line : text.getLines()) {
+    //     Word * temp_word = line->getFinalWord();
+    //     rhymeEngine.matchRhymeToColors(temp_word);
+    //     rhymeEngine.printColorText();
+    //
+    //     }
 
     return 0;
 }
